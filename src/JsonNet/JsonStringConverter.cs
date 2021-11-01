47ede9e05ebac83b7434016564332c43f58753b0
Bastian Burger <22341213+bastbu@users.noreply.github.com>
2021-11-01 19:06:06 +0100

    Add initial version of type safe JSON conversion samples
    Co-authored-by: Atif Aziz <code@raboof.com>

---
+++ src/JsonNet/JsonStringConverter.cs
@@ -0,0 +1,35 @@
+﻿namespace DiscriminatedUnionConverter.JsonNet
+{
+    using System;
+    using Newtonsoft.Json;
+
+    public class JsonStringConverter<T, TParser> : JsonConverter
+        where TParser : IJsonStringParser<T>, new()
+    {
+        private static readonly TParser Parser = new TParser();
+
+        public override bool CanConvert(Type objectType) =>
+            objectType == typeof(T);
+
+        public override object? ReadJson(JsonReader reader, Type objectType, object? existingValue, JsonSerializer serializer) =>
+            reader switch
+            {
+                null => throw new ArgumentNullException(nameof(reader)),
+                { TokenType: JsonToken.Null } => null,
+                { TokenType: JsonToken.String, Value: string s } => Parser.Parse(s),
+                { TokenType: var token, Value: var value } => throw new JsonSerializationException($"Unexpected token or value when parsing version. Token: {token}, Value: {value}")
+            };
+
+        public override void WriteJson(JsonWriter writer, object? value, JsonSerializer serializer)
+        {
+            if (writer is null) throw new ArgumentNullException(nameof(writer));
+
+            writer.WriteValue(Convert.ToString(value, System.Globalization.CultureInfo.InvariantCulture));
+        }
+    }
+
+    public interface IJsonStringParser<out T>
+    {
+        T Parse(string input);
+    }
+}
---
+++ src/SystemTextJson/JsonStringConverter.cs
@@ -0,0 +1,31 @@
+﻿namespace DiscriminatedUnionConverter.SystemTextJson
+{
+    using System;
+    using System.Text.Json;
+    using System.Text.Json.Serialization;
+
+    public class JsonStringConverter<T, TParser> : JsonConverter<T>
+        where TParser : IJsonStringParser<T>, new()
+    {
+        private static readonly TParser Parser = new TParser();
+
+        public override T Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) =>
+            reader.TokenType switch
+            {
+                JsonTokenType.String => Parser.Parse(reader.GetString()),
+                _ => throw new JsonException()
+            };
+
+        public override void Write(Utf8JsonWriter writer, T value, JsonSerializerOptions options)
+        {
+            if (value is null) return;
+
+            writer.WriteStringValue(value.ToString());
+        }
+    }
+
+    public interface IJsonStringParser<out T>
+    {
+        T Parse(string input);
+    }
+}
